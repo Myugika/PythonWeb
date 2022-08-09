4f0509f90a4c7a146d88b35734e28bc8f5fcb9b4
Myugika <silvsk42@gmail.com>
2022-08-09 09:31:18 +0900

    0808

---
+++ 0805 Crawling/0808.py
@@ -0,0 +1,16 @@
+client -> server로 정보 전달할 때
+
+get 방식 : url에 얹어서 보냄
+
+https://www.hanbit.co.kr/academy/books/new_book_list.html
+https://www.hanbit.co.kr/academy/books/new_book_list.html?page=2&cate_cd=&srt=&searchKey=&keyWord=
+
+# 공개되어도 좋은 정보, 4KB 이내로 크기 제한
+
+post 방식 :
+
+ex) 비밀번호, 크기가 큰 데이터
+
+<form>
+
+</form>
